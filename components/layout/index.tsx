f186537b2d71183c956a7b177bd1527884fe358a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-12-11 13:09:38 +0100

    [FEAT] : add 404 page

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -6,7 +6,7 @@ import Navbar from '@/components/navbar';
 interface IProps {
   children: ReactNode;
   title: string;
-  description: string;
+  description?: string;
 }
 
 const Layout: FC<IProps> = ({ title, description, children }) => {
