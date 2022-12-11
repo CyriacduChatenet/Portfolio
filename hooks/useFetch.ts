0e44e6030ed230595a74f1e15a8a5ddcc321fec1
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-12-11 12:42:34 +0100

    [FEAT] : add useFetch hook

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,9 +1,23 @@
-import { useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect } from "react";
 
 interface IProps {};
 
-const useFetch = () => {
-    const fetchData = async () => {};
+const useFetch = (url: string, method: string, dataStateMutator: Dispatch<SetStateAction<{}>>, errorStateMutator: Dispatch<SetStateAction<any>>, body?: Object) => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url, {
+                method,
+                headers: {'content-type': 'application/json'},
+                body: JSON.stringify(body),
+            });
+
+            const responseJSON = await response.json();
+            dataStateMutator(responseJSON);
+        }
+        catch(err) {
+            errorStateMutator(err)
+        }
+    };
 
     useEffect(() => {
         fetchData();
