a5201db11ec79ad8a8b1353d0679f75f5cb71bd2
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-12-13 15:16:27 +0100

    FEAT] : add aniimations

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,4 +1,7 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
+import gsap from "gsap";
+
+import useMenu from "@/contexts/useMenu";
 
 import styles from '@/components/header/style.module.scss';
 
@@ -9,10 +12,28 @@ interface IProps {
 };
 
 const Header: FC<IProps> = ({ title, description}) => {
+    const titleRef = useRef(null);
+    const descriptionRef = useRef(null);
+
+    const { isOpen } = useMenu();
+
+    const handleAnimate = () => {
+        gsap.fromTo(titleRef.current, { opacity:0 }, { opacity:100, duration: 2.5, ease: "power4.in" });
+        gsap.fromTo(descriptionRef.current, { opacity:0 }, { opacity:100, duration: 2.5, ease: "power4.in", delay:1 });
+    };
+
+    if(isOpen === false) {
+        handleAnimate();
+    }
+
+    useEffect(() => {
+        handleAnimate();
+    }, [])
+
     return (
         <header className={styles.container}>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.titleDescription}>{description}</p>
+            <h1 className={styles.title} ref={titleRef}>{title}</h1>
+            <p className={styles.titleDescription} ref={descriptionRef}>{description}</p>
         </header>
     );
 };
