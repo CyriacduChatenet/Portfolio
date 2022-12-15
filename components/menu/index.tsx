27f768658f052748286d5833c067278cc75ca92c
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-12-15 11:44:17 +0100

    [STYLE] : resolve margin

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -3,46 +3,42 @@ import Link from "next/link";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
-import styles from '@/components/menu/style.module.scss';
 import useMenu from "@/contexts/useMenu";
 
+import styles from "@/components/menu/style.module.scss";
+import useAnimateMenu from "@/hooks/animations/useAnimateMenu";
+
+
 const Menu: NextComponentType = () => {
-    const { setIsOpen } = useMenu();
+  const { isOpen, setIsOpen } = useMenu();
 
-    const homeMenuEntry = useRef(null);
-    const projectsMenuEntry = useRef(null);
-    const talksMenuEntry = useRef(null);
-    const aboutMenuEntry = useRef(null);
+  const homeMenuEntry = useRef(null);
+  const projectsMenuEntry = useRef(null);
+  const talksMenuEntry = useRef(null);
+  const aboutMenuEntry = useRef(null);
 
-    const handleAnimate = () => {
-        gsap.fromTo(homeMenuEntry.current, {opacity:0, y:-10}, { opacity:100, y:10, duration: 2.5, ease: "power4.in" });
-        gsap.fromTo(projectsMenuEntry.current, {opacity:0, y:-10}, { opacity:100, y:10, duration: 2.5, ease: "power4.in", delay: 0.5 });
-        gsap.fromTo(talksMenuEntry.current, {opacity:0, y:-10}, { opacity:100, y:10, duration: 2.5, ease: "power4.in", delay: 1  });
-        gsap.fromTo(aboutMenuEntry.current, {opacity:0, y:-10}, { opacity:100, y:10, duration: 2.5, ease: "power4.in", delay: 1.5  });
-    };
+  const menuContainer = useRef(null);
 
-    useEffect(() => {
-        handleAnimate();
-    },[])
+  useAnimateMenu(isOpen, menuContainer, [homeMenuEntry, projectsMenuEntry, talksMenuEntry, aboutMenuEntry]);
 
-    return (
-        <div className={styles.content}>
-            <ul className={styles.navList}>
-                <li>
-                    <Link href={'/'} className={styles.navLink} onClick={() => setIsOpen(false)} ref={homeMenuEntry}>Home</Link>
-                </li>
-                <li>
-                    <Link href={'/projects'} className={styles.navLink} onClick={() => setIsOpen(false)} ref={projectsMenuEntry}>Projects (7)</Link>
-                </li>
-                <li>
-                    <Link href={'/talks'} className={styles.navLink} onClick={() => setIsOpen(false)} ref={talksMenuEntry}>Talks</Link>
-                </li>
-                <li>
-                    <Link href={'/about'} className={styles.navLink} onClick={() => setIsOpen(false)} ref={aboutMenuEntry}>About</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className={styles.content} ref={menuContainer}>
+      <ul className={styles.navList}>
+        <li>
+          <Link href={"/"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={homeMenuEntry} >Home</Link>
+        </li>
+        <li>
+          <Link href={"/projects"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={projectsMenuEntry} >Projects (7)</Link>
+        </li>
+        <li>
+          <Link href={"/talks"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={talksMenuEntry} >Talks</Link>
+        </li>
+        <li>
+          <Link href={"/about"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={aboutMenuEntry}>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Menu;
--- hooks/animations/useAnimateMenu.ts
+++ hooks/animations/useAnimateMenu.ts
@@ -1,20 +1,48 @@
+import { MutableRefObject, useEffect } from "react";
 import gsap from "gsap";
-import { MutableRefObject } from "react";
 
-const useAnimateMenu = (targetState: boolean, element: MutableRefObject<null>) => {
-    const handleAnimateOpen = () => {
-        gsap.fromTo(element.current, {height: 275}, {height: 1000, duration: 2})
-    };
+const useAnimateMenu = (targetState: boolean, background: MutableRefObject<null>, elements: MutableRefObject<null>[]) => {
 
-    const handleAnimateClose = () => {
-        gsap.fromTo(element.current, {height: 1000}, {height: 275, duration: 2})
-    };
+    const handleAnimate = () => {
+        if (targetState) {
+          gsap.fromTo(
+            background.current,
+            { opacity: 0 },
+            { opacity: 100, duration: 2.5, ease: "power4.in" }
+          );
+        }
+        gsap.fromTo(
+          elements[0].current,
+          { opacity: 0, y: -10 },
+          { opacity: 100, y: 10, duration: 2.5, ease: "power4.in" }
+        );
+        gsap.fromTo(
+            elements[1].current,
+          { opacity: 0, y: -10 },
+          { opacity: 100, y: 10, duration: 2.5, ease: "power4.in", delay: 0.5 }
+        );
+        gsap.fromTo(
+            elements[2].current,
+          { opacity: 0, y: -10 },
+          { opacity: 100, y: 10, duration: 2.5, ease: "power4.in", delay: 1 }
+        );
+        gsap.fromTo(
+            elements[3].current,
+          { opacity: 0, y: -10 },
+          { opacity: 100, y: 10, duration: 2.5, ease: "power4.in", delay: 1.5 }
+        );
+      };
+
+      const handleUnAnimate = () => {
+        console.log('unmounted')
+      };
     
-    if(targetState === true) {
-        handleAnimateOpen();
-    } else {
-        handleAnimateClose();
-    }
+      useEffect(() => {
+        handleAnimate();
+        return () => {
+            handleUnAnimate();
+        }
+      }, []);
 };
 
 export default useAnimateMenu;
