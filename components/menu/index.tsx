b7ad0c5c59c75a48bf153847fa24ffa6a61e7217
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-12-17 17:53:49 +0100

    [STYLE] : add menu animation

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,16 +1,13 @@
 import { NextComponentType } from "next";
 import Link from "next/link";
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
+import { useRef } from "react";
 
 import useMenu from "@/contexts/useMenu";
 
 import styles from "@/components/menu/style.module.scss";
 import useAnimateMenu from "@/hooks/animations/useAnimateMenu";
 
-
 const Menu: NextComponentType = () => {
-  const { isOpen, setIsOpen } = useMenu();
 
   const homeMenuEntry = useRef(null);
   const projectsMenuEntry = useRef(null);
@@ -19,22 +16,57 @@ const Menu: NextComponentType = () => {
 
   const menuContainer = useRef(null);
 
-  useAnimateMenu(isOpen, menuContainer, [homeMenuEntry, projectsMenuEntry, talksMenuEntry, aboutMenuEntry]);
+  const { isOpen, setIsOpen } = useMenu();
+
+  useAnimateMenu(isOpen, menuContainer, [
+    homeMenuEntry,
+    projectsMenuEntry,
+    talksMenuEntry,
+    aboutMenuEntry,
+  ]);
 
   return (
     <div className={styles.content} ref={menuContainer}>
       <ul className={styles.navList}>
         <li>
-          <Link href={"/"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={homeMenuEntry} >Home</Link>
+          <Link
+            href={"/"}
+            className={styles.navLink}
+            onClick={() => setIsOpen(false)}
+            ref={homeMenuEntry}
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link href={"/projects"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={projectsMenuEntry} >Projects (7)</Link>
+          <Link
+            href={"/projects"}
+            className={styles.navLink}
+            onClick={() => setIsOpen(false)}
+            ref={projectsMenuEntry}
+          >
+            Projects (7)
+          </Link>
         </li>
         <li>
-          <Link href={"/talks"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={talksMenuEntry} >Talks</Link>
+          <Link
+            href={"/talks"}
+            className={styles.navLink}
+            onClick={() => setIsOpen(false)}
+            ref={talksMenuEntry}
+          >
+            Talks
+          </Link>
         </li>
         <li>
-          <Link href={"/about"} className={styles.navLink} onClick={() => setIsOpen(false)} ref={aboutMenuEntry}>About</Link>
+          <Link
+            href={"/about"}
+            className={styles.navLink}
+            onClick={() => setIsOpen(false)}
+            ref={aboutMenuEntry}
+          >
+            About
+          </Link>
         </li>
       </ul>
     </div>
