0a5375ac5abc806f3f0cf0ef8f5cef956c126479
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-22 16:07:47 +0100

    [FIX] : fix scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { useRef } from "react";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 
 import LoaderProvider from "@/contexts/useLoader";
 import { MenuProvider } from "@/contexts/useMenu";
@@ -17,15 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <LoaderProvider>
       <MenuProvider>
-        <LocomotiveScrollProvider
-          options={options}
-          watch={[]}
-          containerRef={ref}
-        >
-          <div data-scroll-container ref={ref}>
             <Component {...pageProps} />
-          </div>
-        </LocomotiveScrollProvider>
       </MenuProvider>
     </LoaderProvider>
   );
