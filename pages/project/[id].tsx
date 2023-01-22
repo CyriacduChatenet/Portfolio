eaf0578974cbdf9081e399d506487a9c08d2bd6b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-22 19:26:56 +0100

    [FEAT] : add scroll trigger in project page

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 import Layout from "@/components/layout";
 import useMenu from "@/contexts/useMenu";
