97a389933d0f5c9b9de75cef5d83f5f6be825407
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-23 19:54:04 +0100

    [FIX] : fix files

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -1,10 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Dispatch, SetStateAction, useEffect } from "react";
 
-import { Data } from "@/types/data";
 
-
-const useFetch = (url: string, method: string, dataStateMutator: Dispatch<SetStateAction<Data>>, errorStateMutator: Dispatch<SetStateAction<any>>, body?: Object) => {
+const useFetch = (url: string, method: string, dataStateMutator: Dispatch<SetStateAction<any>>, errorStateMutator: Dispatch<SetStateAction<any>>, body?: Object) => {
     const fetchData = async () => {
         try {
             const response = await fetch(url, {
