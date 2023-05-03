484f318c479061fca050c79ef2f83d6fa1174965
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-03 16:46:19 +0200

    [FIX] files

---
+++ client/api/wakatime/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const getData = async (): Promise<any> => {
+    const response = await axios(`${process.env.WAKATIME_URL}`);
+    return response;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const response = await getData();
+    res.write(JSON.stringify(response.data));
+    res.end();
+}
